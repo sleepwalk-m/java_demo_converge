8e54ed4cf208612a1eb88f2af0d1f5278a49d9d0
Mask <74847491+sleepwalk-m@users.noreply.github.com>
2021-07-27 11:52:07 +0800

    Update JobPageProcessor.java
    添加了header伪装，解决详情页无法采集的问题

--- crawler-job/src/main/java/com/mask/ssm/job/compoent/JobPageProcessor.java
+++ crawler-job/src/main/java/com/mask/ssm/job/compoent/JobPageProcessor.java
@@ -109,11 +109,31 @@ public class JobPageProcessor implements PageProcessor {
             Map<String,Object> map = new HashMap<String, Object>();
             map.put("level","detail"); // 标识位
             request.setExtras(map);
+            setGetHeaders(request);
             page.addTargetRequest(request);
         }
     }
 
     public Site getSite() {
         return Site.me();
+    }
+    
+    
+    /**
+     * 添加请求伪装
+     *
+     * @param request
+     */
+    public void setGetHeaders(Request request){
+        request.addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+        request.addHeader("Accept-Encoding","gzip, deflate, br");
+        request.addHeader("Accept-Language","zh-CN,zh;q=0.9");
+        request.addHeader("Cache-Control","max-age=0");
+        request.addHeader("Connection","keep-alive");
+        request.addHeader("Host","jobs.51job.com");
+        request.addHeader("Referer","https://search.51job.com/");
+        request.addHeader("Upgrade-Insecure-Requests","1");
+        request.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+        request.addHeader("Cookie","_uab_collina=162735205034353766732154; guid=12b7d58b5754c9671e042cfed72e6e23; nsearch=jobarea%3D%26%7C%26ord_field%3D%26%7C%26recentSearch0%3D%26%7C%26recentSearch1%3D%26%7C%26recentSearch2%3D%26%7C%26recentSearch3%3D%26%7C%26recentSearch4%3D%26%7C%26collapse_expansion%3D; search=jobarea%7E%60080200%7C%21ord_field%7E%600%7C%21recentSearch0%7E%60080200%A1%FB%A1%FA000000%A1%FB%A1%FA0000%A1%FB%A1%FA32%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA9%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA0%A1%FB%A1%FAJava%BF%AA%B7%A2%A1%FB%A1%FA2%A1%FB%A1%FA1%7C%21; acw_tc=76b20ffa16273520460128704e64a6cf35d46409e3087dfc0883c673442d46; acw_sc__v2=60ff6beec5252ed092fcb6a4a427bc48ffefc9bf; ssxmod_itna=eqfxBDy70QdYq0Kq0dD=wgDSid+kxY5eD7YEFiRDBL74iNDnD8x7YDvIILQYvCYgG0oeLKUYTYG6trfpbvq1jRENQDU4i8DCM2eYTDen=D5xGoDPxDeDADYE6DAqiOD7qDdfhTXtkDbxi3fxiaDGeDeEKODY5DhxDC2mPDwx0Cfx24mA9hO6BCTKy45t0DfxG1a40HeASINU8LmmyhwFSQ4xGdDpMDImdeeQiYDU4ODl92DCF1uEyFTIkkMO5GVW23tW2e/BD3ONiote+xZix485QoP7P45DrPpWxb77aKDDc4GOpDD=; ssxmod_itna2=eqfxBDy70QdYq0Kq0dD=wgDSid+kxY5eD7YEFiD8T648xGXhPqGafWUsh1fx82FjxVfWkDoOV4aqxQbVScD9+8WcWogbq0i7mSFktzK4IXw9loISLOtLZnxQnqjri9Tu999=ZuIeG+3RDQ6pYw3sFAYYCd22I3iI8nhzYkYuSfolnf2U94XUtf2PkW0bpeXX3L5s3+jWpQ=AfgcsAbuOVCFRQM5DkfyeL2DaD7QmD7=DewexD===");
     }
 }
