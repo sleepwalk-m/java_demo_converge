9b305c906c73150a06fff1665136b2a497ab39be
Mask <jiangf616@163.com>
2021-11-21 16:33:00 +0800

    springboot 整合 drools

---
+++ drools_springboot/src/main/java/com/drools/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.drools.controller;
+
+import com.drools.service.RuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @Autowired
+    private RuleService ruleService;
+    @RequestMapping("/rule")
+    public String rule(){
+        ruleService.rule();
+        return "OK";
+    }
+}
