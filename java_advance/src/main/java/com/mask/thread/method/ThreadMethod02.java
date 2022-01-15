3cefed575fa745f1b6369add8c6d4522ae539ffd
wuchang <1830605377@qq.com>
2022-01-15 23:24:12 +0800

    wuchang 多线程方法演示2

---
+++ java_advance/src/main/java/com/mask/thread/method/ThreadMethod02.java
@@ -0,0 +1,13 @@
+package com.mask.thread.method;
+
+/**
+ * @author: Mask.m
+ * @date: 2022/1/15 23:23
+ * @version: v1.0
+ * @Description: 线程的方法演示2
+ */
+public class ThreadMethod02 {
+    public static void main(String[] args) {
+
+    }
+}
